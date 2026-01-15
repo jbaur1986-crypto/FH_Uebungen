6a41c36fbe79f7db80d13eaaf682276fc234d488
Johannes Baur <j.baur1986@gmail.com>
2026-01-15 10:18:30 +0100

    Teilversion01

---
+++ Dateien Zeiterfassung Schwimmbad/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Dateien_Zeiterfassung_Schwimmbad;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    decimal KundenUmsatz(string kundenid, out int dauer)
+    {
+        string verzeichnis = "C:/DevProjects/csharp/Dateien Zeiterfassung Schwimmbad/Zeiterfassung.txt";
+        
+        using (StreamReader sr = new StreamReader(verzeichnis))
+        {
+            while (!sr.EndOfStream)
+            {
+                string zeile = sr.ReadLine();
+                string[] teile = zeile.Split('\t');
+                string zeiterfassungkundenid = teile[0];
+
+                dauer = 0;
+                if (zeiterfassungkundenid == kundenid && teile[3] != null)
+                {
+                    string beginn = teile[2];
+                    string ende = teile[3];
+                    string[] beginnstundenminuten = beginn.Split(':');
+                    string[] endestundenminuten = ende.Split(':');
+                    int[] beginnzeit = { int.Parse(beginnstundenminuten[0]), int.Parse(beginnstundenminuten[1]) };
+                    int[] endezeit = { int.Parse(endestundenminuten[0]), int.Parse(endestundenminuten[1]) };
+                    //hier wird noch die Dauer berechnet:
+                }
+            }
+        }
+        //hier wird aus der Dauer die Kosten berechnet:
+        return //Kosten;
+    }
+}
