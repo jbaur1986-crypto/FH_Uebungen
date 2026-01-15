ded4dedde25cc8e64023f1ce4f642f4f6267d9a2
Johannes Baur <j.baur1986@gmail.com>
2026-01-15 11:23:55 +0100

    Final

--- Dateien Zeiterfassung Schwimmbad/Program.cs
+++ Dateien Zeiterfassung Schwimmbad/Program.cs
@@ -5,12 +5,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        decimal dummy = KundenUmsatz("K125", out int lang);
+        Console.WriteLine($"{dummy}");
     }
 
-    decimal KundenUmsatz(string kundenid, out int dauer)
+    static decimal KundenUmsatz(string kundenid, out int gesamtdauer)
     {
         string verzeichnis = "C:/DevProjects/csharp/Dateien Zeiterfassung Schwimmbad/Zeiterfassung.txt";
+        gesamtdauer = 0;
         
         using (StreamReader sr = new StreamReader(verzeichnis))
         {
@@ -20,20 +22,29 @@ class Program
                 string[] teile = zeile.Split('\t');
                 string zeiterfassungkundenid = teile[0];
 
-                dauer = 0;
-                if (zeiterfassungkundenid == kundenid && teile[3] != null)
+             
+                if (zeiterfassungkundenid == kundenid && teile.Length >=4)
                 {
+                    int dauer = 0;
                     string beginn = teile[2];
                     string ende = teile[3];
                     string[] beginnstundenminuten = beginn.Split(':');
                     string[] endestundenminuten = ende.Split(':');
-                    int[] beginnzeit = { int.Parse(beginnstundenminuten[0]), int.Parse(beginnstundenminuten[1]) };
-                    int[] endezeit = { int.Parse(endestundenminuten[0]), int.Parse(endestundenminuten[1]) };
-                    //hier wird noch die Dauer berechnet:
+                    int[] beginnzeitgeteilt = { int.Parse(beginnstundenminuten[0]), int.Parse(beginnstundenminuten[1]) };
+                    int[] endezeitgeteilt = { int.Parse(endestundenminuten[0]), int.Parse(endestundenminuten[1]) };
+                    int beginnzeit = beginnzeitgeteilt[0] * 60 + beginnzeitgeteilt[1];
+                    int endezeit = endezeitgeteilt[0] * 60 + endezeitgeteilt[1];
+                    //hier wird noch die Dauer/Min berechnet:
+                    dauer = endezeit - beginnzeit;
+                    gesamtdauer += dauer;
                 }
             }
         }
-        //hier wird aus der Dauer die Kosten berechnet:
-        return //Kosten;
+        //hier wird aus der Dauer die Kosten berechnet:Noch nicht korrekt! Angefangene Viertelstunde: 0,40€
+        double gesamt = 0;
+        gesamt = Math.Ceiling((double)gesamtdauer / 15d);
+        decimal kosten;
+        kosten = (decimal)gesamt * 0.4m;
+        return kosten; 
     }
 }
